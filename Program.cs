da16258b6236d112b0829eea2e6c6a8a50fe50e9
Matteo Imbimbo <matteo.imbimbo@yahoo.it>
2022-05-18 14:42:48 +0200

    Added functions

--- Program.cs
+++ Program.cs
@@ -38,9 +38,6 @@ namespace csharp_snacks_2 // Note: actual namespace depends on the project name.
 
 
 
-
-
-
             // Data una lista di interi, metterli tutti al quadrato
 
             List<int> list = new List<int>() { 2,3,4,5,6,7,8,9 };
@@ -189,16 +186,46 @@ namespace csharp_snacks_2 // Note: actual namespace depends on the project name.
             };
 
 
-            lcoppie.Sort(Comparer<Tuple<string, int>>.Default);
-            foreach (Tuple<string, int> str in lcoppie)
-                Console.WriteLine(str);
+            lcoppie.Sort((s2, s1) => s2.Item1.CompareTo(s1.Item1));
+            foreach (Tuple<string, int> s in lcoppie)
+                Console.WriteLine($"{ s.Item1} con  { s.Item2}");
+
+            //lcoppie.Sort(Comparer<Tuple<string, int>>.Default);
+            //foreach (Tuple<string, int> str in lcoppie)
+            //    Console.WriteLine(str);
+
+            lcoppie.ForEach(x => Console.WriteLine(x));
 
 
 
+            Console.WriteLine("\n\n\n\n\nLCOPPIE");
+            lcoppie.Sort();
+            lcoppie.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine(String.Join("\t", lcoppie));
+            //Ok, allora ordiniamo per il secondo campo della tutpla (il numero intero)
+            lcoppie.Sort((t1, t2) => t1.Item2.CompareTo(t2.Item2));
+            Console.WriteLine(String.Join("\t", lcoppie));
+            lcoppie.Sort();
+            lcoppie.Sort((t1, t2) => t1.Item2 - t2.Item2);
+            Console.WriteLine(String.Join("\t", lcoppie));
+            List<Tuple<int, int, int>> lterne = new List<Tuple<int, int, int>>()
+            {
+                new Tuple<int, int, int>(1, 2, 3),
+                new Tuple<int, int, int>(5, 5, 2),
+                new Tuple<int, int, int>(2, 4, 11),
+                new Tuple<int, int, int>(12, 15, 21),
+                new Tuple<int, int, int>(55, 45, 32),
+                new Tuple<int, int, int>(1, 2, 4),
+                new Tuple<int, int, int>(1, 3, 0),
+                new Tuple<int, int, int>(5, 5, 1)
+            };
+            lterne.Sort();
+            Console.WriteLine(String.Join("\t", lterne));
 
 
-
-
+            // UTILE PER VERIFICARE LA VELOCITA DEL CODICE. Calcola i millisecondi prima di eseguire il ciclo e dopo l'esecuzione del ciclo 
+            double microseconds = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0);
+            Console.WriteLine("microseconds: {0}", microseconds);
 
             //IMPORTANTISSIMO = La funzione "Select" equivale alla funzione Map in javascript e la funzione "Where" equivale alla funzione Filter in javascript
 
